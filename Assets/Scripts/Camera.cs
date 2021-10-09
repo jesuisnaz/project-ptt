612880a23a4f7c40755937a9e44619369ae390b7
Nazarii Klymok <nazarii.klymok@botscrew.com>
2021-10-09 19:54:38 +0300

    added mapping camera into boundaries

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    [SerializeField] private Transform target;
+    [SerializeField] private float smoothing;
+    [SerializeField] private Vector2 minPos;
+    [SerializeField] private Vector2 maxPos;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        if (transform.position != target.position) {
+            Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            targetPos.x = Mathf.Clamp(targetPos.x, minPos.x, maxPos.x);
+            targetPos.y = Mathf.Clamp(targetPos.y, minPos.y, maxPos.y);
+
+            transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
+        }
+    }
+}
