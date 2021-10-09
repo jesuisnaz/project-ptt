6a680c4ea58c954f088c1c78243b873e81a7172a
Nazarii Klymok <nazarii.klymok@botscrew.com>
2021-10-09 18:56:53 +0300

    comment fixes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,36 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private const float Tolerance = 0.1f;
+    private const string HorizontalAxisName = "Horizontal";
+    private const string VerticalAxisName = "Vertical";
 
-    public float moveSpeed = 5f;
-    public Transform movePoint;
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Transform movePoint;
     
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         movePoint.parent = null;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, movePoint.position) <= .05f)
+        if (Vector3.Distance(transform.position, movePoint.position) <= Tolerance)
         {
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) != 0f &&
-                Mathf.Abs(Input.GetAxisRaw("Vertical")) != 0f) return;
-
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
-            {
-                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+            if (Mathf.Abs(Input.GetAxisRaw(HorizontalAxisName)) != 0f &&
+                Mathf.Abs(Input.GetAxisRaw(VerticalAxisName)) != 0f) {
+                return;
             }
-            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+
+            if (Mathf.Abs(Input.GetAxisRaw(HorizontalAxisName) - 1f) < Tolerance)
             {
-                movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                movePoint.position += new Vector3(Input.GetAxisRaw(HorizontalAxisName), 0f, 0f);
+            }
+            if (Mathf.Abs(Input.GetAxisRaw(VerticalAxisName) - 1f) < Tolerance)
+            {
+                movePoint.position += new Vector3(0f, Input.GetAxisRaw(VerticalAxisName), 0f);
             }
         }
     }
