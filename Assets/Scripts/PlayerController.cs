992687ead5beb675c86419f33e1014849c10e25e
Nazarii Klymok <nazarii.klymok@botscrew.com>
2021-10-11 15:28:51 +0300

    added map bounds for player in playerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Transform _movePoint;
+    [SerializeField] private Vector2 _minPoint;
+    [SerializeField] private Vector2 _maxPoint;
 
     private const float Tolerance = 0.1f;
     private const string HorizontalAxisName = "Horizontal";
@@ -17,6 +19,13 @@ public class PlayerController : MonoBehaviour
                Mathf.Abs(verticalAxis) != 0f;
     }
 
+    private bool isWithinBounds(float deltaHorizontal, float deltaVertical) {
+        float afterMoveHorizontal = gameObject.transform.position.x + deltaHorizontal;
+        float afterMoveVertical = gameObject.transform.position.y + deltaVertical;
+        return _minPoint.x <= afterMoveHorizontal && afterMoveHorizontal <= _maxPoint.x &&
+            _minPoint.y <= afterMoveVertical && afterMoveVertical <= _maxPoint.y;
+    }
+
     private void Update()
     {
         float horizontalAxisInput = Input.GetAxisRaw(HorizontalAxisName);
@@ -24,11 +33,11 @@ public class PlayerController : MonoBehaviour
         if (!isCurrentlyMoving(horizontalAxisInput, verticalAxisInput) &&
             Vector3.Distance(transform.position, _movePoint.position) <= Tolerance)
         {
-            if (Mathf.Abs(horizontalAxisInput) == 1f)
+            if (Mathf.Abs(horizontalAxisInput) == 1f && isWithinBounds(horizontalAxisInput, verticalAxisInput))
             {
                 OnMoveChange(new DirectionWrapper(HorizontalAxisName, horizontalAxisInput));
             }
-            if (Mathf.Abs(verticalAxisInput) == 1f)
+            if (Mathf.Abs(verticalAxisInput) == 1f && isWithinBounds(horizontalAxisInput, verticalAxisInput))
             {
                 OnMoveChange(new DirectionWrapper(VerticalAxisName, verticalAxisInput));
             }
