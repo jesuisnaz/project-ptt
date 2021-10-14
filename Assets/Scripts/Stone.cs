8ba5e17693c1406a8ad3aa4500cbe221d2f21815
Bogdan Didukh <bdidukh@tsukat.com>
2021-10-14 11:31:17 +0300

    Add stone logic

---
+++ Assets/Scripts/Stone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Stone : MonoBehaviour, IUnit
+{
+    [SerializeField] private Vector3 _currentPosition;
+
+    public Vector3 CurrentPosition => _currentPosition;
+
+    private void Start()
+    {
+        transform.position = _currentPosition;
+    }
+}
