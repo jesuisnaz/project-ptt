40672b7f9a3e67688c1072154fbc727bb4a57ee8
BogdanDidukh2003 <diduhbogdan441@gmail.com>
2021-10-16 17:59:48 +0300

    Upd project

---
+++ Assets/Scripts/ScaredHuman.cs
@@ -0,0 +1,67 @@
+﻿
+using UnityEngine;
+
+public class ScaredHuman : MonoBehaviour,  IUnit
+{
+    public Vector3 CurrentPosition { get; private set; }
+
+    [SerializeField] private float _moveSpeed = 15f;
+    [SerializeField] private Transform _movePoint;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private ScaredHumanController _scaredHumanController;
+    [SerializeField] private SoundManager _soundManager;
+    private DirectionWrapper _currentDirection;
+
+
+    private void Awake()
+    {
+        _scaredHumanController.OnMoveChange += Move;
+        _scaredHumanController.OnAnimationChange += SetAnimator;
+        _scaredHumanController.OnTeleport += Teleport;
+    }
+
+    private void OnDestroy()
+    {
+        _scaredHumanController.OnMoveChange -= Move;
+        _scaredHumanController.OnAnimationChange -= SetAnimator;
+        _scaredHumanController.OnTeleport -= Teleport;
+    }
+
+    private void Start()
+    {
+        _movePoint.parent = null;
+        CurrentPosition = _movePoint.position;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _movePoint.position, _moveSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // if (other.gameObject.CompareTag("Pumpkin")) {
+        //     _pumpkinManager.PickUpPumpkin(other.gameObject);
+        //     _soundManager.playSound("PumpkinPickedUp");      
+        // }
+        Debug.Log("collision with object: " + other.tag);
+    }
+
+    public void Move(DirectionWrapper directionWrapper)
+    {
+        _movePoint.position += directionWrapper.Vector3Value;
+        CurrentPosition = _movePoint.position;
+    }
+
+    public void SetAnimator(DirectionWrapper horizontalDirectionWrapper, DirectionWrapper verticalDirectionWrapper)
+    {
+        _animator.SetFloat(horizontalDirectionWrapper.AxisName, horizontalDirectionWrapper.AxisValue);
+        _animator.SetFloat(verticalDirectionWrapper.AxisName, verticalDirectionWrapper.AxisValue);
+    }
+
+    public void Teleport(Vector3 position)
+    {
+        transform.position = position;
+        CurrentPosition = position;
+    }
+}
---
+++ Assets/Scripts/ScaredHumanController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ScaredHumanController : MonoBehaviour
+{
+    private const int MaxXPosition = 31;
+    private const int MaxYPosition = 17;
+    private const float Tolerance = 0.1f;
+    private const string HorizontalAxisName = "Horizontal";
+    private const string VerticalAxisName = "Vertical";
+    
+    [SerializeField] private ScaredHuman _scaredHuman;
+    [SerializeField] private Transform _movePoint;
+    [SerializeField] private Vector2 _minMapCordinatesPoint;
+    [SerializeField] private Vector2 _maxMapCordinatesPoint;
+    [SerializeField] private Stone[] _stones;
+    private Vector3 _goalPoint;
+    
+    public Action<DirectionWrapper> OnMoveChange = delegate {};
+    public Action<DirectionWrapper, DirectionWrapper> OnAnimationChange = delegate {};
+    public Action<Vector3> OnTeleport = delegate {};
+    
+    private void Update()
+    {
+        ControlCommonMovement();
+    }
+
+    private void Start()
+    {
+        GenerateRandomPoint();
+    }
+
+    private bool IsCurrentlyMoving(DirectionWrapper horizontalDirectionWrapper, DirectionWrapper verticalDirectionWrapper)
+    {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          return Mathf.Abs(horizontalDirectionWrapper.AxisValue) != 0f && Mathf.Abs(verticalDirectionWrapper.AxisValue) != 0f;
+    }
+
+    private bool IsLegalMove(DirectionWrapper horizontalDirectionWrapper, DirectionWrapper verticalDirectionWrapper) {
+        float afterMoveHorizontal = _scaredHuman.CurrentPosition.x + horizontalDirectionWrapper.AxisValue; 
+        float afterMoveVertical = _scaredHuman.CurrentPosition.y + verticalDirectionWrapper.AxisValue;
+        Vector3 afterMovePosition = new Vector3(afterMoveHorizontal, afterMoveVertical);
+        bool isWithinBounds = _minMapCordinatesPoint.x <= afterMoveHorizontal &&
+                              afterMoveHorizontal <= _maxMapCordinatesPoint.x &&
+                              _minMapCordinatesPoint.y <= afterMoveVertical &&
+                              afterMoveVertical <= _maxMapCordinatesPoint.y;
+        bool isNotStuckIntoStone = true;
+        foreach (var stone in _stones)
+        {
+            if (stone.CurrentPosition == afterMovePosition)
+            {
+                isNotStuckIntoStone = false;
+            }
+        }
+        
+        return isWithinBounds && isNotStuckIntoStone;
+    }
+
+    private void ControlCommonMovement()
+    {
+        int distanceToXCord = (int)_goalPoint.x - (int)_scaredHuman.CurrentPosition.x;
+        int distanceToYCord = (int)_goalPoint.y - (int)_scaredHuman.CurrentPosition.y;
+        if (distanceToXCord == 0 && distanceToYCord == 0)
+        {
+            GenerateRandomPoint();
+            return;
+        }
+
+        DirectionWrapper horizontalDirectionWrapper = new DirectionWrapper(HorizontalAxisName, Mathf.Sign(distanceToXCord));
+        DirectionWrapper verticalDirectionWrapper = new DirectionWrapper(VerticalAxisName, Mathf.Sign(distanceToYCord));
+
+        bool isMovingHorizontally = Convert.ToBoolean(Random.Range(0, 1));
+        
+        if (horizontalDirectionWrapper.AxisValue == 0)
+        {
+            isMovingHorizontally = false;
+        }
+        
+        if (verticalDirectionWrapper.AxisValue == 0)
+        {
+            isMovingHorizontally = true;
+        }
+        
+        if (!IsCurrentlyMoving(horizontalDirectionWrapper, verticalDirectionWrapper) &&
+            Vector3.Distance(transform.position, _movePoint.position) <= Tolerance)
+        {
+            if (IsLegalMove(horizontalDirectionWrapper, verticalDirectionWrapper))
+            {
+                if (isMovingHorizontally)
+                {
+                    OnMoveChange(horizontalDirectionWrapper);
+                }
+                else
+                {
+                    OnMoveChange(verticalDirectionWrapper);
+                }
+            }
+            OnAnimationChange(horizontalDirectionWrapper, verticalDirectionWrapper);
+        }
+    }
+    
+    private void GenerateRandomPoint()
+    {
+        int x = Random.Range(0, MaxXPosition), y = Random.Range(0, MaxYPosition), z = 0;
+        _goalPoint= new Vector3(x, y, z);
+        if (Random.Range(0, 10) == 1)
+        {
+            OnTeleport(_goalPoint);
+            GenerateRandomPoint();
+        }
+    }
+}
