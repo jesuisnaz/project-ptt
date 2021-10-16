30208da0c02090f1982acc3a60edc1ece52c8c07
Nazarii Klymok <nazarii.klymok@botscrew.com>
2021-10-16 20:45:21 +0300

    added finish, replaced menu_bg, added responsiveness

--- Assets/Scripts/PumpkinManager.cs
+++ Assets/Scripts/PumpkinManager.cs
@@ -1,14 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PumpkinManager : MonoBehaviour
 {
     [SerializeField] private GameObject _PumpkinPrefab;
     [SerializeField] private GameObject _Parent;
     [SerializeField] private GameObject _Player;
-    [SerializeField] private const int _MaxPumpkinCount = 5;
-    [SerializeField] private const int _SpawnMagnitude = 3;
+    [SerializeField] private int _MaxPumpkinCount = 5;
+    [SerializeField] private int _SpawnMagnitude = 3;
     [SerializeField] private const int _MaxXPosition = 31;
     [SerializeField] private const int _MaxYPosition = 17;
     [SerializeField] private TMPro.TextMeshProUGUI _ScoreText;
@@ -39,6 +40,9 @@ public class PumpkinManager : MonoBehaviour
         if (_PumpkinCount%10 == 0)
         {
             player.SetCanTransform(true);
+        }
+        if (_PumpkinCount >= 100) {
+            SceneManager.LoadScene("Finish");
         }
     }
 
